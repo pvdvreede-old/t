0fcd1b7686596a2cb5cfcc073c42352ce0376ab6
Paul Van de Vreede <paul@vdvreede.net>
2011-06-22 12:18:46 +1000

    Added files for file import page. #23

---
+++ src/Vdvreede/TFrontendBundle/Controller/ImportController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Vdvreede\TFrontendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ImportController extends Controller
+{
+    public function indexAction()
+    {
+        
+    }
+    
+}
---
+++ src/Vdvreede/TFrontendBundle/Form/ImportFileType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Vdvreede\TFrontendBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Vdvreede\TFrontendBundle\Entity;
+
+class ImportFileType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder->add('file', 'file');
+        $builder->add('import_name', 'entity', array(
+            'class' => 'Vdvreede\\TFrontendBundle\\Entity\\TransImport',
+            'query_builder' => function($er) {
+                return $er->createQueryBuilder('i')
+                          ->where('userId = :userId)
+                          ->andWhere('accountId = :accountId')
+                          ->setParameter('userId', 1)
+                          ->setParameter('accountId', 1);
+             }
+        ));	
+
+    }
+}
