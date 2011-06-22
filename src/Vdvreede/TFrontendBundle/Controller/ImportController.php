4d1a9e578b24de448361a86b450212ff561f350e
Paul Van de Vreede <paul@vdvreede.net>
2011-06-22 12:28:27 +1000

    Added route for file import and renamed the controller action to file to distinguish. #23

--- src/Vdvreede/TFrontendBundle/Controller/ImportController.php
+++ src/Vdvreede/TFrontendBundle/Controller/ImportController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ImportController extends Controller
 {
-    public function indexAction()
+    public function fileAction()
     {
         
     }
