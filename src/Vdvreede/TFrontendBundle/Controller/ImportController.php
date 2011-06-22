355a14fbbc16bb0a352c0bc6c5fe4981d96ddfbd
Paul Van de Vreede <paul@vdvreede.net>
2011-06-22 18:23:42 +1000

    Started coding controller for import csv page. #24

--- src/Vdvreede/TFrontendBundle/Controller/ImportController.php
+++ src/Vdvreede/TFrontendBundle/Controller/ImportController.php
@@ -8,7 +8,7 @@ class ImportController extends Controller
 {
     public function fileAction($accountId)
     {       
-	$file = new Entity\ImportFile();
+	
         
         $form = $this->createForm(new Form\ImportFileType(), new Entity\ImportFile());
 
@@ -18,7 +18,26 @@ class ImportController extends Controller
     }
 
     public function csvAction($accountId) {
+       
+      $file = new Entity\ImportFile(); 
+      $form = $this->createForm(new Form\ImportFileType(), $file);
 
+      $request = $this->get('request');
+
+      $form->bindRequest($request);
+
+      if ($form->isValid()) {
+	$form['file']->move('/home/paul/', 'temp.csv');
+     
+        $handle = fopen('temp.csv', 'r');
+        
+        $i = 0;
+        $count = 0;
+        while ($data = fgetcsv($handle, 1000, ',') !== false && $i < 6) {
+          $count = count($data);
+
+        }
+      }
     }
     
 }
