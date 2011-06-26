9f20f4e5b9a26a057073a1d718e79ac47f10c382
Paul Van de Vreede <paul@vdvreede.net>
2011-06-26 10:17:10 +1000

    Fixed views across transaction and account, fixed form error in account. #7 #5

--- src/Vdvreede/TFrontendBundle/Form/AccountType.php
+++ src/Vdvreede/TFrontendBundle/Form/AccountType.php
@@ -4,15 +4,19 @@ namespace Vdvreede\TFrontendBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use Vdvreede\TFrontendBundle\Entity\Account;
 
-class AccountType extends AbstractType
-{
-    public function buildForm(FormBuilder $builder, array $options)
-    {
+class AccountType extends AbstractType {
+
+    public function buildForm(FormBuilder $builder, array $options) {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('type')
-        ;
+                ->add('name')
+                ->add('description')
+                ->add('type', 'choice', array(
+                    'choices' => array(
+                        Account::$SAVINGS => 'Savings',
+                        Account::$TRANSACTION => 'Transaction'
+                        )));
     }
+
 }
