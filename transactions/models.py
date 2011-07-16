57536fd9457204d4a1862b9f78aa939a657ff203
Paul Van de Vreede <paul@vdvreede.net>
2011-07-16 14:39:25 +1000

    Added initials transaction models.

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    type = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    colour = models.CharField(max_length=10)
+    reportable = models.BooleanField()
+    
+    def __unicode__(self):
+        return self.name
+    
+class Transaction(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    reportable = models.BooleanField()
+    account = models.ForeignKey(Account)
+    category = models.ForeignKey(Category)
+    
+    def __unicode__(self):
+        return self.description
