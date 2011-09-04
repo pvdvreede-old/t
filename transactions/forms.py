cd8237b37c57abaa95d6b4bc10bc7b36a39086a2
Paul Van de Vreede <paul@vdvreede.net>
2011-09-04 22:23:15 +1000

    Various changes including adding better form rendering with validation.

--- transactions/forms.py
+++ transactions/forms.py
@@ -1,11 +1,21 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateField
 from t.transactions.models import *
+
+class UserModelForm(ModelForm):
+    
+    def clean_user(self):
+        self.user=self.request.user
+    
+    class Meta:
+        includes=('name')
+        excludes=('description')
 
 class TransactionForm(ModelForm):
     class Meta:
         model=Transaction
-        
-        
-class CategoryForm(ModelForm):
+
+               
+class CategoryForm(UserModelForm):
     class Meta:
         model=Category
+        includes=('name')
--- transactions/views.py
+++ transactions/views.py
@@ -1,15 +1,27 @@
 from django.views.generic import *
 from t.transactions.models import *
 from t.transactions.forms import *
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 class AccountLoader():   
     def get_accounts(user_id):
         accounts = Account.objects.filter(user_id=user_id)       
         return accounts
 
-class TransactionsListView(ListView, AccountLoader):
+class TransactionsListView(ListView):
     model=Transaction
     template_name="transaction_list.html" 
+    paginated_by=2
+    
+   ## @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(TransactionsListView, self).dispatch(*args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super(TransactionsListView, self).get_context_data(**kwargs)
+        context['url'] = 'transaction'
+        return context
     
     def get_queryset(self):
         return Transaction.objects.filter(user=self.request.user)
@@ -30,6 +42,11 @@ class TransactionEditView(UpdateView):
 class CategoryListView(ListView):
     model=Category
     template_name="transaction_list.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['url'] = 'category'
+        return context
     
     def get_queryset(self):
         return Category.objects.filter(user=self.request.user)
