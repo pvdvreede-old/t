7b1a204abfcd784dfa92185afbaa0037ebd3be76
pvdvreede <paul@vdvreede.net>
2011-09-06 12:07:49 +1000

    Changed static url function to helper function rather than one using hardcoding of static locations.

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from t.transactions.views import *
 
 from django.contrib import admin
@@ -24,7 +24,4 @@ urlpatterns = patterns('',
     
 )
 
-if settings.DEBUG:
-    urlpatterns += patterns('django.contrib.staticfiles.views',
-        url(r'^static/(?P<path>.*)$', 'serve'),
-    )
+urlpatterns += staticfiles_urlpatterns()
