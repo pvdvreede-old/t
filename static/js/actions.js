85a421875f5a0a0d458d6f0d6c0296c37e5afde1
Paul Van de Vreede <paul@vdvreede.net>
2011-09-06 22:43:04 +1000

    Moved js file up to top level and added in function to close message box.

--- static/js/actions.js
+++ static/js/actions.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
 	$("a.dropdown-toggle").click(function() {
 		$(this).parent().toggleClass("open");
 	});
+	
+	$("a.close").click(function() {
+		$(this).parent().remove();
+	});
 
 });
