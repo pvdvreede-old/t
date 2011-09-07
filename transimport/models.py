74e71246aafd473422c6326f9ca39f3473f023ba
Paul Van de Vreede <paul@vdvreede.net>
2011-09-07 18:00:33 +1000

    Add staging model to transimport.

--- transimport/models.py
+++ transimport/models.py
@@ -12,3 +12,14 @@ class TransImport(models.Model):
     
     def __unicode__(self):
         return self.name
+        
+class TransStaging(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User)
+    status = models.IntegerField()
+    created_date = models.DateTimeField()
+    
+    def __unicode__(self):
+        return self.description
