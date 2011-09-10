fbe23d1a5a13e78e9a2dc803362c1b690440ef03
Paul Van de Vreede <paul@vdvreede.net>
2011-09-10 14:21:30 +1000

    Alter models for better integrity and extra fields.

--- transactions/models.py
+++ transactions/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from datetime import datetime
 
 class AccountType(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(blank=True, null=True, max_length=300)
     created_date = models.DateTimeField(editable=False, auto_now_add=True)
     modified_date = models.DateTimeField(null=False, editable=False, auto_now=True)
     
@@ -13,23 +14,31 @@ class AccountType(models.Model):
 class Account(models.Model):
     name = models.CharField(max_length=50)
     account_type = models.ForeignKey(AccountType)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     user = models.ForeignKey(User)
     created_date = models.DateTimeField(editable=False, auto_now_add=True)
     modified_date = models.DateTimeField(null=False, editable=False, auto_now=True)
     
     def __unicode__(self):
         return self.name
+        
+    class Meta:
+	unique_together = ("name", "user", "account_type")
     
 class Category(models.Model): 
     name = models.CharField(max_length=50)
     colour = models.CharField(max_length=10)
     reportable = models.BooleanField()
+    spent = models.DecimalField(max_digits=10, decimal_places=2, editable=False, default=0.00)
     user = models.ForeignKey(User)
     created_date = models.DateTimeField(editable=False, auto_now_add=True)
     modified_date = models.DateTimeField(null=False, editable=False, auto_now=True)
     
     def __unicode__(self):
         return self.name
+        
+    class Meta:
+	unique_together = ("name", "user")
     
 class Transaction(models.Model):
     date = models.DateField()
