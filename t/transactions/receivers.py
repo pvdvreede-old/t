8b295cb3e24853ec0173117e429c57fba7d784b8
Paul Van de Vreede <paul@vdvreede.net>
2011-09-14 17:52:45 +1000

    Add query for budget update.

--- t/transactions/receivers.py
+++ t/transactions/receivers.py
@@ -39,5 +39,44 @@ def recalculate_amount(instance):
     instance.account.balance = amount["amount__sum"]
     instance.account.save()
     
+def recalcuate_budget(instance):
+	from django.db import connection, transaction
+	cursor = connection.cursor
+	cursor.execute("""
+		insert into budget_budgetitem (budget, user, category, spent, limit, balance, start_date, end_date)
+		select b.id, b.user, b.category, b.limit, b.limit, b.limit, 
+		CASE 
+		  WHEN b.type = 'Yearly' THEN MAKEDATE( EXTRACT(YEAR FROM '{0}'),1);
+		  WHEN b.type = 'Monthly' THEN DATE_FORMAT(CONCAT(SUBSTR('{0}',1,8),'01'),'%a');
+		  WHEN b.type = 'Weekly' THEN SUBDATE('{0}', INTERVAL 8 DAY) AND DAYOFWEEK({date}) >= 1;
+		END CASE,
+		CASE 
+		  WHEN b.type = 'Yearly' THEN STR_TO_DATE(CONCAT(12,31,EXTRACT(YEAR FROM {date})), '%Y-%m-%d') ;
+		  WHEN b.type = 'Monthly' THEN DATE_FORMAT(LAST_DAY('{0}'),'%a');
+		  WHEN b.type = 'Weekly' THEN SUBDATE('{0}', INTERVAL 8 DAY) AND DAYOFWEEK(column1) >= 1;
+		END CASE,
+		from budget_budget b
+		left join budget_budgetitem bi on b.user = bi.user and b.category = bi.category
+		where bi.start_date <= '{0}'
+		and b.user = {1}
+		and b.category = {2}
+		and bi.end_date >= '{0}'
+		and bi.id is null
+	""".format(instance.date, instance.user, instance.category))
 	
+	cursor.execute("""
+		update budget_budgetitem bi
+		set spent = (select SUM(t.amount)
+					 from transactions_transaction t
+					 where t.user = {1}
+					  and t.category = {2}
+					  and t.date >= bi.start_date
+					  and t.date <= bi.end_date)
+			,balance = limit - spent
+		where bi.user = {1}
+		 and bi.category = {2}
+		 and bi.start_date <= '{0}'
+		 and bi.end_date >= '{0}'
+	""".format(instance.date, instance.user, instance.category))
+	transaction.commit_unless_managed()
 	
